16aba2b8af217a3612dad0193d87ba611fa9edc7
MarianaAntunesJ <marianajantunes97@gmail.com>
2021-05-03 12:39:01 -0300

    Design completo

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -5,7 +5,11 @@ namespace ModernDesign.MVVM.ViewModel
 {
     class MainViewModel :ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
         public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
 
         private object _currentView;
 
@@ -20,6 +24,19 @@ namespace ModernDesign.MVVM.ViewModel
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
         }
     }
 }
