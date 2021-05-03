031702ef4bd389bed21fc301ad049a43bd017d9f
MarianaAntunesJ <marianajantunes97@gmail.com>
2021-05-03 12:18:55 -0300

    Designs gerais

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ModernDesign.Core;
+using System;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    class MainViewModel :ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
