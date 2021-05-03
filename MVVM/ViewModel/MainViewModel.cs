0c9953152eb73ae7fb332d8bd5e36c9cb8983750
MarianaAntunesJ <marianajantunes97@gmail.com>
2021-05-03 12:26:15 -0300

    MainViewModel implementação

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -5,9 +5,21 @@ namespace ModernDesign.MVVM.ViewModel
 {
     class MainViewModel :ObservableObject
     {
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainViewModel()
         {
-
+            HomeVM = new HomeViewModel();
         }
     }
 }
